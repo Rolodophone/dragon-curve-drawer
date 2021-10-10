7348dbc3dc8b959398077950ddeb8f2ffde55bb7
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-10-10 22:28:05 +0100

    ability to do more than one dragon at a time

--- DragonCurveDrawer.py
+++ DragonCurveDrawer.py
@@ -1,111 +1,124 @@
 import colorsys
+import json
 import turtle
-# import threading
+import threading
+import queue
+import sys
+import time
 
 
 def main():
-    iterations, distance, speed, extraSpeed, colourmode, variation = config()
+    iterations, distance, delay, speed, extraSpeed, colourMode, variation, startXs, startYs, rotations, simultaneousDrawing, numOfDragons \
+        = config()
 
-    # if advancedMode and simultaneousDragoning:
-    #     for i in range(numOfDragons):
-    #         newT = turtle.Turtle()
-    #         newTS = newT.getscreen()
-    #         newT.speed = speed
-    #         newTS.colormode(1.0)
-    #         newT.hideturtle()
-    #
-    #         threading.Thread(target=lambda: drawDragon(newT, iterations, distance, colourmode, variation, startXs[i], startYs[i], rotations[i])).start()
-    # else:
-    newT = turtle.Turtle()
-    newTS = newT.getscreen()
-    newT.speed = speed
-    newTS.delay(0)
-    newTS.tracer(n=extraSpeed)
-    newTS.colormode(1.0)
-    newT.hideturtle()
+    commandQueue = queue.Queue(1)
+    threads = []
 
-    drawDragon(newT, iterations, distance, colourmode, variation)
+    for i in range(numOfDragons):
+        newT = turtle.Turtle()
+        newTS = newT.getscreen()
+        newT.speed = speed
+        newTS.delay(delay)
+        newTS.tracer(n=extraSpeed)
+        newTS.colormode(1.0)
+        newT.hideturtle()
 
-    input("Press enter to exit")
+        thread = threading.Thread(target=lambda:
+            drawDragon(newT, iterations, distance, colourMode, variation, startXs[i], startYs[i], rotations[i], commandQueue))
+        thread.daemon = True  # if main thread dies early (e.g. close button pressed), drawing threads will die
+        thread.start()
+        threads.append(thread)
+
+    processQueue(commandQueue)
+
+    # wait until drawing is finished
+    for thread in threads:
+        thread.join()
+
+
+def processQueue(commandQueue):
+    while not commandQueue.empty():
+        commandQueue.get()()  # invoke command
+
+    time.sleep(0.1)
+    processQueue(commandQueue)
+
+    print("Finished drawing.")
 
 
 def config():
-    # I couldn't get threading to work with the turtle to draw simultaneously so I disabled this functionality
+    fileName = None
 
-    # ans = input("Multi-dragon mode (y/n)? ")
-    # if ans == "y":
-    #     advancedMode = True
-    # elif ans == "n":
-    # advancedMode = False
-    # else:
-    #     print("Invalid input\n\n\n")
-    #     config()
-    #
-    # if advancedMode:
-    #     numOfDragons = int(input("How many dragons? "))
-    #     ans = input("Draw simultaneously (y/n)? ")
-    #     if ans == "y":
-    #         simultaneousDragoning = True
-    #     elif ans == "n":
-    #         simultaneousDragoning = False
-    #     else:
-    #         print("Invalid input\n\n\n")
-    #         config()
-    # else:
-    # numOfDragons = 1
-    # simultaneousDragoning = False
+    if len(sys.argv) >= 2:
+        fileName = sys.argv[1]
+    else:
+        ans = input("Load config from file (y/N)? ")
+        if ans == "y":
+            fileName = input("Enter file name, including extension: ")
 
-    # if (advancedMode):
-    #     print("\nFor all dragons...")
+    if fileName is not None:
+        with open(fileName, "r") as file:
+            d = json.load(file)
+
+        return d["iterations"], d["distance"], d["delay"], d["speed"], d["extraSpeed"], d["colourMode"], d["variation"], d["startXs"], \
+            d["startYs"], d["rotations"], d["simultaneousDrawing"], d["numOfDragons"]
+
+    ans = input("Multi-dragon mode (y/N)? ")
+    if ans == "y":
+        advancedMode = True
+    else:
+        advancedMode = False
+
+    if advancedMode:
+        numOfDragons = int(input("How many dragons? "))
+        ans = input("Draw simultaneously (Y/n)? ")
+        if ans == "n":
+            simultaneousDrawing = False
+        else:
+            simultaneousDrawing = True
+    else:
+        numOfDragons = 1
+        simultaneousDrawing = False
+
+    if advancedMode:
+        print("\nFor all dragons...")
+
     iterations = int(input("How many iterations? "))
     distance = int(input("Side length in pixels? "))
+    delay = int(input("Delay? "))
     speed = int(input("Speed (0-10)? "))
     extraSpeed = int(input("Draw how many lines at a time (1+)? "))
-    colourmode = input("Colour mode (none/global rainbow/local rainbow)? ")
+    colourMode = input("Colour mode (none/global rainbow/local rainbow)? ")
     variation = input("Variation (none/onoff)? ")
-    if not (colourmode == "none" or colourmode == "global rainbow" or colourmode == "local rainbow") or not (variation == "none" or variation == "onoff"):
-        print("Invalid input\n\n\n")
-        config()
 
-    # ts = turtle.Turtle().getscreen()
-    #
-    # if advancedMode:
-    #     startXs = []
-    #     startYs = []
-    #     rotations = []
-    #     for i in range(numOfDragons):
-    #         print("\nFor dragon #{}...".format(i+1))
-    #
-    #         ans = input("Starting x position? ")
-    #         if ans == "default":
-    #             startXs.append(ts.window_width() / 2)
-    #         else:
-    #             startXs.append(int(ans))
-    #
-    #         ans = input("Starting y position? ")
-    #         if ans == "default":
-    #             startYs.append(ts.window_height() / 2)
-    #         else:
-    #             startYs.append(int(ans))
-    #
-    #         ans = input("Starting rotation? ")
-    #         if ans == "default":
-    #             rotations.append(0)
-    #         else:
-    #             rotations.append(ans)
-    #
-    # else:
-    #     startXs = [ts.window_width() / 2]
-    #     startYs = [ts.window_height() / 2]
-    #     rotations = [0]
+    if not (colourMode == "none" or colourMode == "global rainbow" or colourMode == "local rainbow") or not (variation == "none" or variation == "onoff"):
+        print("Invalid input")
+        exit()
 
-    return iterations, distance, speed, extraSpeed, colourmode, variation  #, startXs, startYs, rotations
+    if advancedMode:
+        startXs = []
+        startYs = []
+        rotations = []
+        for i in range(numOfDragons):
+            print("\nFor dragon #{}...".format(i+1))
+            startXs.append(int(input("Starting x position? ")))
+            startYs.append(int(input("Starting y position? ")))
+            rotations.append(int(input("Starting rotation? ")))
+
+    else:
+        startXs = [960]
+        startYs = [540]
+        rotations = [0]
+
+    return iterations, distance, delay, speed, extraSpeed, colourMode, variation, startXs, startYs, rotations, \
+        simultaneousDrawing, numOfDragons
 
 
-def drawDragon(t, iterations, distance, colourmode, variation):
-    # t.goto(startX, startY)
-    # t.setheading(rotation)
-    t.forward(distance)
+def drawDragon(t: turtle.Turtle, iterations: int, distance: int, colourMode: str, variation: str, startX: int, startY: int, rotation: int,
+               commandQueue: queue.Queue):
+    commandQueue.put(lambda: t.goto(startX, startY))
+    commandQueue.put(lambda: t.setheading(rotation))
+    commandQueue.put(lambda: t.forward(distance))
 
     oddLine = True
 
@@ -114,38 +127,39 @@ def drawDragon(t, iterations, distance, colourmode, variation):
     for i in range(iterations):
         newMovesToCopy = list(movesToCopy)
 
-        if colourmode == "global rainbow":
-            t.color(colorsys.hsv_to_rgb((i / iterations), 0.8, 0.8))
+        if colourMode == "global rainbow":
+            commandQueue.put(lambda: t.color(colorsys.hsv_to_rgb((i / iterations), 0.8, 0.8)))
 
         if i != 0:
-            t.right(90)
+            commandQueue.put(lambda: t.right(90))
             newMovesToCopy.insert(0, -1)
-            t.forward(distance)
+            commandQueue.put(lambda: t.forward(distance))
 
         for i, move in enumerate(movesToCopy):
-            if colourmode == "local rainbow":
-                t.color(colorsys.hsv_to_rgb((i / len(movesToCopy)), 0.8, 0.8))
+            if colourMode == "local rainbow":
+                commandQueue.put(lambda: t.color(colorsys.hsv_to_rgb((i / len(movesToCopy)), 0.8, 0.8)))
 
             if variation == "onoff":
                 if oddLine:
                     oddLine = False
-                    t.penup()
+                    commandQueue.put(lambda: t.penup())
                 else:
                     oddLine = True
-                    t.pendown()
-
+                    commandQueue.put(lambda: t.pendown())
 
             if move == 1:
-                t.right(90)
+                commandQueue.put(lambda: t.right(90))
                 newMovesToCopy.insert(0, -1)
 
             elif move == -1:
-                t.left(90)
+                commandQueue.put(lambda: t.left(90))
                 newMovesToCopy.insert(0, 1)
 
-            t.forward(distance)
+            commandQueue.put(lambda: t.forward(distance))
 
         movesToCopy = list(newMovesToCopy)
 
 
-main()
+if __name__ == "__main__":
+    main()
+    input("Press enter to exit.")  # keep window open when turtle finishes
